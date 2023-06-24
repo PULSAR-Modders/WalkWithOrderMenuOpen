c62eaf85c70f7c428ec7249571cae87dc2860f77
18107 <18107@users.noreply.github.com>
2023-06-24 19:18:33 +0930

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace Walk_with_order_menu_open
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Allows the player to continue moving while using the order menu";
+
+        public override string Name => "Walk with order menu open";
+
+        public override string ModID => "walkwithordermenuopen";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.walkwithordermenuopen";
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using static PulsarModLoader.Patches.HarmonyHelpers;
+
+namespace Walk_with_order_menu_open
+{
+    [HarmonyPatch(typeof(PLPlayerController), "HandleMovement")]
+    internal class PlayerController
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            // && !PLInput.Instance.GetButton(PLInputBase.EInputActionName.order_menu)
+            List<CodeInstruction> targetSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldsfld), //PLInput.Instance
+                new CodeInstruction(OpCodes.Ldc_I4_S), //PLInputBase.EInputActionName.order_menu
+                new CodeInstruction(OpCodes.Callvirt), //GetButton()
+                new CodeInstruction(OpCodes.Brtrue) //&& !
+            };
+
+            //This list intentionally left blank
+            List<CodeInstruction> patchSequence = new List<CodeInstruction>();
+
+            return PatchBySequence(instructions, targetSequence, patchSequence, PatchMode.REPLACE, CheckMode.NEVER);
+        }
+    }
+}
